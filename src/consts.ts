d33890acdd4369cea905bd33c9969d97b8ea49d5
Benzagon <gonzabenzaquen@gmail.com>
2025-08-27 09:23:55 -0300

    fix: consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "BoulderTech Maintenance";
-export const SITE_DESCRIPTION = "App is under maintenance, try again in a bit.";
+export const SITE_TITLE = "BoulderTech Coming Soon";
+export const SITE_DESCRIPTION = "App will be launching soon.";
